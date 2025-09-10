69484535da3fe0b104cb612a4e9c28d83aff20ed
Jon Luu <j_luu3@yahoo.com>
2025-09-10 16:24:50 -0700

    Initial Commit

--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! One more Modification");
+﻿void MyFirstMethod()
+{
+    //do something
+}
+void MyFirstMethod()
+{
+    //do something
+}
+void MyFirstMethod()
+{
+    //do something
+}
